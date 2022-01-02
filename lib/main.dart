0ca6ecd2c319b60260687f9595520c1af7cf450b
cjohnson <collin.g.johnson@gmail.com>
2022-01-01 20:38:56 -0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(children: const <Widget>[
+        Card(
+          child: Text('CHART'),
+        ),
+        Card(
+          child: Text('LIST OF TX')
+        )
+      ],)
+    );
+  }
+}
+
