196a0db45a26bc89565dcc7e2cba3f9682380a31
cjohnson <collin.g.johnson@gmail.com>
2022-01-02 21:39:17 -0500

    Use built-in string interpolation feature instead of string concatenation

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyHomePage extends StatelessWidget {
                 ),
                 padding: EdgeInsets.all(10),
                 child: Text(
-                  tx.amount.toString(),
+                  '\$${tx.amount}',
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
@@ -73,10 +73,10 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(tx.title,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                   Text(tx.date.toString(),
-                    style: TextStyle(color: Colors.grey),
+                    style: const TextStyle(color: Colors.grey),
                   )
                 ],
               )
